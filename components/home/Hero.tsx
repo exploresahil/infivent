5186a01a0d2db782e5519554fdb726f2d1af0718
Renuka Fuse <renukafuse17@gmail.com>
2023-07-20 18:14:04 +0530

    updated

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,7 +1,32 @@
+import Image from "next/image";
 import React from "react";
+import bg from "@/public/png/hero-bg.png"
+import heroimg from "@/public/png/Mask group.png"
 
 const Hero = () => {
-  return <div>Home Hero</div>;
+  return <div className="hero-section">
+    <div className="bg-container">
+      <Image fill alt="" src={bg} />
+    </div>
+    <div className="hero-text">
+      <h1>
+        Streamline, connect & elevate b2b events & expos!
+      </h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus in mollis nunc sed id semper risus in.
+      </p>
+      <button>Get Started</button>
+    </div>
+    <div className="heroimg-cont">
+      <div className="hero-img">
+        <Image fill alt="" src={heroimg} />
+      </div>
+
+
+    </div>
+
+
+  </div>;
+
 };
 
 export default Hero;
---
+++ components/home/Solution.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Solution = () => {
+    return (
+        <div className='solution-section'>
+            <h2>Solutions</h2>
+            <div className='grid-container'>
+                <div className='grid'>
+                    <h3>
+                        Event Creation and Management
+                    </h3>
+                    <ul>
+                        <li>Create and manage multiple events and expos from a single platform.</li>
+                        <li>Set event dates, times and locations.</li>
+                        <li>Define event categories andtracks.</li>
+
+                    </ul>
+
+                </div>
+                <div className='grid'>
+                    <h3>
+                        Event Creation and Management
+                    </h3>
+                    <ul>
+                        <li>Create and manage multiple events and expos from a single platform.</li>
+                        <li>Set event dates, times and locations.</li>
+                        <li>Define event categories andtracks.</li>
+
+                    </ul>
+
+                </div>
+                <div className='grid'>
+                    <h3>
+                        Event Creation and Management
+                    </h3>
+                    <ul>
+                        <li>Create and manage multiple events and expos from a single platform.</li>
+                        <li>Set event dates, times and locations.</li>
+                        <li>Define event categories andtracks.</li>
+
+                    </ul>
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Solution
