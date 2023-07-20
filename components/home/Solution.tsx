6aa0ec5ed7fb2554a3b6d68506abb693b2f90d6d
Sahil Satpute <explore.sahil1@gmail.com>
2023-07-20 18:35:45 +0530

    updated

--- components/home/Solution.tsx
+++ components/home/Solution.tsx
@@ -1,50 +1,68 @@
-import React from 'react'
+import React from "react";
 
 const Solution = () => {
-    return (
-        <div className='solution-section'>
-            <h2>Solutions</h2>
-            <div className='grid-container'>
-                <div className='grid'>
-                    <h3>
-                        Event Creation and Management
-                    </h3>
-                    <ul>
-                        <li>Create and manage multiple events and expos from a single platform.</li>
-                        <li>Set event dates, times and locations.</li>
-                        <li>Define event categories andtracks.</li>
-
-                    </ul>
-
-                </div>
-                <div className='grid'>
-                    <h3>
-                        Event Creation and Management
-                    </h3>
-                    <ul>
-                        <li>Create and manage multiple events and expos from a single platform.</li>
-                        <li>Set event dates, times and locations.</li>
-                        <li>Define event categories andtracks.</li>
-
-                    </ul>
-
-                </div>
-                <div className='grid'>
-                    <h3>
-                        Event Creation and Management
-                    </h3>
-                    <ul>
-                        <li>Create and manage multiple events and expos from a single platform.</li>
-                        <li>Set event dates, times and locations.</li>
-                        <li>Define event categories andtracks.</li>
-
-                    </ul>
-
-                </div>
-            </div>
-
+  return (
+    <div className="solution-section">
+      <h2>Solutions</h2>
+      <div className="grid-container">
+        <div className="grid">
+          <h4>Event Creation and Management</h4>
+          <ul>
+            <li>
+              Create and manage multiple events and expos from a single
+              platform.
+            </li>
+            <li>Set event dates, times and locations.</li>
+            <li>Define event categories andtracks.</li>
+          </ul>
         </div>
-    )
-}
+        <div className="grid">
+          <h4>Event Creation and Management</h4>
+          <ul>
+            <li>
+              Create and manage multiple events and expos from a single
+              platform.
+            </li>
+            <li>Set event dates, times and locations.</li>
+            <li>Define event categories andtracks.</li>
+          </ul>
+        </div>
+        <div className="grid">
+          <h4>Event Creation and Management</h4>
+          <ul>
+            <li>
+              Create and manage multiple events and expos from a single
+              platform.
+            </li>
+            <li>Set event dates, times and locations.</li>
+            <li>Define event categories andtracks.</li>
+          </ul>
+        </div>
+        <div className="grid">
+          <h4>Event Creation and Management</h4>
+          <ul>
+            <li>
+              Create and manage multiple events and expos from a single
+              platform.
+            </li>
+            <li>Set event dates, times and locations.</li>
+            <li>Define event categories andtracks.</li>
+          </ul>
+        </div>
+        <div className="grid">
+          <h4>Event Creation and Management</h4>
+          <ul>
+            <li>
+              Create and manage multiple events and expos from a single
+              platform.
+            </li>
+            <li>Set event dates, times and locations.</li>
+            <li>Define event categories andtracks.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Solution
+export default Solution;
